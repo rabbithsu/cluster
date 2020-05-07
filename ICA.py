3ddfd274a455a2e2bf52b683af0e95f48c98d974
alexhsu <alexhsu@fortinet.com>
2020-05-07 11:00:31 +0800

    recover

--- ICA.py
+++ ICA.py
@@ -3,9 +3,9 @@ import pandas
 
 data_set = pandas.read_csv("0504_behavior.csv")
 features = data_set.drop("User_ID", axis = 1)
-print features
+
 
 transformer = FastICA(n_components=3, random_state=0)
 X_transformed = transformer.fit_transform(features)
-X_transformed.shape
-print transformer.predict(features)
+print X_transformed
+print X_transformed.shape
